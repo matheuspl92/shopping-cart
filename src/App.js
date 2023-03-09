718c38830875acc37356405e24378dc1b76824f2
Matheus <matheuspl92@hotmail.com>
2023-03-09 18:21:11 -0300

    Refactor code to fix Nav link errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Nav from "./components/Nav/Nav";
-import RouteSwitch from "./components/RouteSwitch/RouteSwitch";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <RouteSwitch />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<ShoppingCart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
