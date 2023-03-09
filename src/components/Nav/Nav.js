67e0460697afe1b433d8660a4ed23bf2b7acc0c2
Matheus <matheuspl92@hotmail.com>
2023-03-09 18:21:59 -0300

    Add links to nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <nav>
+            <h1>ShoppingCart</h1>
+            <ul>
+                <Link to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link to='/cart'>
+                    <li>Cart</li>
+                </Link>
+            </ul>
+        </nav>
+    )
 }
