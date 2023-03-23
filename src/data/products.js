bd514301e5554c28ba8f650d52363330a4e1263f
Matheus <matheuspl92@hotmail.com>
2023-03-23 19:51:50 -0300

    Add products data

---
+++ src/data/products.js
@@ -0,0 +1,64 @@
+const products = [
+    {
+        cardName: 'AORUS Radeon™ RX 7900 XTX ELITE 24G',
+        cardPrice: 1999,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/04a28aa202dec679ec59dbd21bc71aad/Product/32850/webp'
+    },
+    {
+        cardName: 'Radeon™ RX 7900 XTX GAMING OC 24G',
+        cardPrice: 1899,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/df68c5863e046427671d3cf6d9a4d6e4/Product/32888/webp'
+    },
+    {
+        cardName: 'Radeon™ RX 7900 XT GAMING OC 20G',
+        cardPrice: 1699,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/0b56655c1f1f40d6a6a610bdff4b931c/Product/32879/webp'
+    },
+    {
+        cardName: 'Radeon™ RX 7900 XTX 24G',
+        cardPrice: 1799,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/ffebdb331cdc6e8eecf7f2b4b42b8232/Product/32793/webp'
+    },
+    {
+        cardName: 'Radeon™ RX 7900 XT 20G',
+        cardPrice: 1649,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/868436ba12e23fc0d98e26322a618a2c/Product/32810/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6950 XT XTREME WATERFORCE WB 16G',
+        cardPrice: 1499,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/1575ca3107ada01c47255e7580a30d42/Product/31524/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6900 XT XTREME WATERFORCE WB 16G',
+        cardPrice: 1399,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/c973b83ea7dadc0cc9c571ceaf5df41d/Product/28768/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6900 XT MASTER 16G (rev. 2.0)',
+        cardPrice: 1399,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/9a461e1f617f0a688cf6c5d9f2a792ed/Product/30367/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6900 XT MASTER 16G (rev. 1.0)',
+        cardPrice: 1349,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/e059e84029f763c411bdeb89afece25a/Product/27371/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6800 XT MASTER TYPE C 16G',
+        cardPrice: 1299,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/cb505a967bb885840298241dad4b764a/Product/26951/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6800 XT MASTER 16G',
+        cardPrice: 1249,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/987573c08d11833a015940343c9746dd/Product/26818/webp'
+    },
+    {
+        cardName: 'AORUS Radeon™ RX 6800 MASTER 16G',
+        cardPrice: 999,
+        cardImg: 'https://static.gigabyte.com/StaticFile/Image/Global/165a7f0a578fdbebb6e16e5d038d2170/Product/26804/webp'
+    },
+];
+
+export default products;
