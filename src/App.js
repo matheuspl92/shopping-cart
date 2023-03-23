97a3a699901d25e220314c5377bcdc7394cdca4c
Matheus <matheuspl92@hotmail.com>
2023-03-23 20:38:45 -0300

    Add cart state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,14 +8,22 @@ import Cart from "./components/Cart/Cart";
 import Shop from "./components/Shop/Shop";
 
 function App() {
+  const { cart, setCart } = useState([]);
+
+  const addToCart = (order) => {
+    let [ ...newState ] = cart;
+    newState.push(order);
+    setCart(newState);
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path='/' element={<Home cart={cart} />} />
+          <Route path='/shop' element={<Shop cart={cart} addToCart={addToCart}/>} />
+          <Route path='/cart' element={<Cart cart={cart} />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,14 +3,14 @@ import { Container, Row } from 'react-bootstrap'
 import products from '../../data/products'
 import ItemCard from '../Card/ItemCard'
 
-export default function Shop() {
+export default function Shop({ cart, addToCart }) {
     return (
         <div>
             <h2>Shop</h2>
             <Container>
                 <Row>
                     {products.map( product => {
-                        return <ItemCard title={product.cardName} text={`Price: $ ${product.cardPrice}`} srcImage={product.cardImg} />
+                        return <ItemCard title={product.cardName} text={`Price: $ ${product.cardPrice}`} srcImage={product.cardImg} addToCart={addToCart} />
                     })}
                 </Row>
             </Container>
