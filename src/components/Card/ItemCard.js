552407bd8f585f371e5861fb3697e68f40208130
Matheus <matheuspl92@hotmail.com>
2023-03-23 19:31:56 -0300

    Add quantity input to ItemCard components

--- src/components/Card/ItemCard.js
+++ src/components/Card/ItemCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, FormControl } from 'react-bootstrap';
 
-export default function ItemCard( { title = 'Card Title', text = "Some quick example text to build on the card title and make up the bulk of the card's content.", srcImage = "holder.js/100px180"} ) {
+export default function ItemCard({ title = 'Card Title', text = "Some quick example text to build on the card title and make up the bulk of the card's content.", srcImage = "holder.js/100px180" }) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={srcImage} />
@@ -10,7 +10,10 @@ export default function ItemCard( { title = 'Card Title', text = "Some quick exa
         <Card.Text>
           {text}
         </Card.Text>
-        <Button variant="primary">Add to cart</Button>
+        <div>
+          <FormControl type='number' placeholder='Quantity' />
+          <Button variant="primary">Add to cart</Button>
+        </div>
       </Card.Body>
     </Card>
   )
