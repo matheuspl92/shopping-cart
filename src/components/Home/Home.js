3a25197bce46ae2054bc288fb5f55a5939c7313a
Matheus <matheuspl92@hotmail.com>
2023-03-24 19:24:20 -0300

    Style nav bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <h2>Home</h2>
+    <div>
+      <section id='top'>
+        <h2>Unleash the Red Team power!</h2>
+      </section>
+      <section id='middle'>
+        <h2>Amazing selection of cards</h2>
+      </section>
+      <section id='bottom'>
+        <h2>Contact us!</h2>
+      </section>
+    </div>
   )
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,48 +1,28 @@
-import React, { useRef } from 'react';
-import { Overlay } from 'react-bootstrap';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav({ cart }) {
-    const target = useRef(null);
 
     return (
         <nav>
-            <h1>StoreName</h1>
-            <ul>
+            <h1 id='store-name'><strong style={{ color: 'red' }}>Red</strong>Team</h1>
+            <ul id='link-list'>
                 <Link to='/'>
                     <li>Home</li>
                 </Link>
                 <Link to='/shop'>
                     <li>Shop</li>
                 </Link>
-                <Link to='/cart'>
-                    <li ref={target} >Cart</li>
-                    <Overlay target={target.current} show={true} placement="right">
-                        {({
-                            placement: _placement,
-                            arrowProps: _arrowProps,
-                            show: _show,
-                            popper: _popper,
-                            hasDoneInitialMeasure: _hasDoneInitialMeasure,
-                            ...props
-                        }) => (
-                            <div
-                                {...props}
-                                style={{
-                                    position: 'absolute',
-                                    backgroundColor: 'rgba(255, 100, 100, 0.85)',
-                                    padding: '2px 10px',
-                                    color: 'white',
-                                    borderRadius: 3,
-                                    ...props.style,
-                                }}
-                            >
-                                {cart.length}
-                            </div>
-                        )}
-                    </Overlay>
-                </Link>
+                <div id="cart-div">
+                    <Link to='/cart'>
+                        <li>Cart</li>
+                    </Link>
+                    <div id='cart-display'>
+                        {cart.length}
+                    </div>
+                </div>
+
             </ul>
         </nav>
     )
