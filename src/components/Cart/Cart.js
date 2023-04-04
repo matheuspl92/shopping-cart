de765d303dbde760af13ded36c43ba5c9e45e44f
Matheus <matheuspl92@hotmail.com>
2023-04-04 20:31:14 -0300

    Add basic styling to cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Row } from 'react-bootstrap';
 import OrderCard from '../Order/OrderCard';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Cart({ cart }) {
   return (
     <div>
-      <h2>Cart</h2>
-      <Container>
-        {cart.map((order) => {
-          console.log(order)
-          return <OrderCard key={uuidv4()} orderData={order} />
-        })}
+      <h2>Your cart</h2>
+      <Container fluid id='shop-container'>
+        <Row className='d-flex justify-content-center p-5'>
+          {cart.length ? cart.map((order) => {
+            console.log(order)
+            return <OrderCard key={uuidv4()} orderData={order} />
+          }) : <h3>Things are pretty empty around here...</h3>}
+        </Row>
       </Container>
+      <Button size='lg' variant='dark'>Checkout</Button>
     </div>
   )
 }
--- src/components/Order/OrderCard.js
+++ src/components/Order/OrderCard.js
@@ -3,13 +3,10 @@ import { Card } from 'react-bootstrap';
 
 export default function OrderCard({ orderData }) {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '12rem' }}>
             <Card.Img variant="top" src={orderData.srcImage + '/300'} />
             <Card.Body>
                 <Card.Title>{orderData.title}</Card.Title>
-                <Card.Text>
-                    {orderData.text}
-                </Card.Text>
                 <Card.Text>
                     {`Quantity: ${orderData.orderQtd}`}
                 </Card.Text>
