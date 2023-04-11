784e0dff71ea1834a898440a41ebe7b83074e946
Matheus <matheuspl92@hotmail.com>
2023-04-10 21:15:59 -0300

    Refactor and add styling to Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Button, Container, Row } from 'react-bootstrap';
 import OrderCard from '../Order/OrderCard';
 import { v4 as uuidv4 } from 'uuid';
+import './Cart.css';
 
 export default function Cart({ cart, removeFromCart }) {
   return (
     <div>
       <h2>Your cart</h2>
-      <Container fluid id='shop-container'>
-        <Row className='d-flex justify-content-center p-5'>
+      <Container id='shop-container'>
+        <Row className='d-flex justify-content-center p-5 gap-5'>
           {cart.length ? cart.map((order) => {
             console.log(order)
             return <OrderCard key={uuidv4()} orderData={order} removeFromCart={removeFromCart} />
