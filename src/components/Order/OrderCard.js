d87ec2eb6315e5ad19937a4d4471d52f124fbcff
Matheus <matheuspl92@hotmail.com>
2023-04-10 21:14:19 -0300

    Refactor and add styling to OrderCard component

--- src/components/Order/OrderCard.js
+++ src/components/Order/OrderCard.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Container, Image } from 'react-bootstrap';
+import './OrderCard.css';
 
 export default function OrderCard({ orderData, removeFromCart }) {
     return (
-        <Card style={{ width: '12rem' }}>
-            <Card.Img variant="top" src={orderData.srcImage + '/300'} />
-            <Card.Body>
-                <Card.Title>{orderData.title}</Card.Title>
-                <Card.Text>
-                    {`Quantity: ${orderData.orderQtd}`}
-                </Card.Text>
+        <Container className='order-card justify-content-between align-items-center'>
+            <div className='img-and-info'>
+                <Image src={orderData.srcImage + '/300'} alt='Product' />
+                <div>
+                    <p>{orderData.title}</p>
+                    <p>{`Unit price: ${orderData.price}`}</p>
+                    <p>{`Order quantity: ${orderData.orderQtd}`}</p>
+                </div>
+            </div>
+            <div className="total-and-button">
+                <p>{`Order total: ${orderData.orderQtd * orderData.price}`}</p>
                 <Button variant='light' onClick={() => {
-                    removeFromCart(orderData.id);
-                }}>Remove order</Button>
-            </Card.Body>
-        </Card>
+                        removeFromCart(orderData.id);
+                    }}>Remove order</Button>
+            </div>
+        </Container>
     )
 }
+
