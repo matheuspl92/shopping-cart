4a145389b6ac783ff27c522d1822c20863b28b77
Matheus <matheuspl92@hotmail.com>
2023-04-13 22:14:01 -0300

    Refactor and style Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <section id='bottom'>
-        <Button variant='light'>Subscribe to newsletter</Button>
-        <p>Terms and Conditions | Privacy | Trademarks | Cookies Policy | Cookies Settings</p>
-        <p>© 2023 RedTeam Online Store</p>
-      </section>
+    <footer id='bottom'>
+      <Button variant='light'>Subscribe to newsletter</Button>
+      <ul className='footer-list'>
+        <li>
+          <a className='footer-links' href="#">Terms and Conditions</a>
+        </li>
+        <li>|</li>
+        <li>
+          <a className='footer-links' href="#">Privacy</a>
+        </li>
+        <li>|</li>
+        <li>
+          <a className='footer-links' href="#">Trademarks</a>
+        </li>
+        <li>|</li>
+        <li>
+          <a className='footer-links' href="#">Cookies Policy</a>
+        </li>
+        <li>|</li>
+        <li>
+          <a className='footer-links' href="#">Cookies Settings</a>
+        </li>
+      </ul>
+      <p>© 2023 RedTeam Online Store</p>
+    </footer>
   )
 }
