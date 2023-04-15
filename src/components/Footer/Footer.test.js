88dc1985442af7b9f88a91e29ae160f3ae866163
Matheus <matheuspl92@hotmail.com>
2023-04-14 21:25:02 -0300

    Add Footer component tests

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getAllByRole, render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Footer from "./Footer";
+
+describe('Footer component', () => {
+
+    test('displays "Subscribe to newsletter" button', () => {
+        render(<Footer />);
+        const buttonElement = screen.getByRole('button', { name: 'Subscribe to newsletter' });
+
+        expect(buttonElement).toBeInTheDocument();
+    });
+
+    test('displays all links', () => {
+        render(<Footer />);
+        const linkElements = screen.getAllByRole('link');
+
+        linkElements.map( element => {
+            expect(element).toBeInTheDocument();
+        })
+    });
+
+    test('displays copyright text', () => {
+        render(<Footer />);
+        const textElement = screen.getByText('© 2023 RedTeam Online Store');
+
+        expect(textElement).toBeInTheDocument();
+    })
+})
